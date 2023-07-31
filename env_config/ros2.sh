5639f8ab5f655c07035988e655a953679050c00e
Minh Nguyen <1168534+minhnh@users.noreply.github.com>
2023-07-31 18:02:54 +0200

    change to LST ROS2 version

--- env_config/ros2.sh
+++ env_config/ros2.sh
@@ -1,9 +1,9 @@
 export ROS_VERSION=2
 export ROS_PYTHON_VERSION=3
-export ROS_DISTRO=iron
+export ROS_DISTRO=humble
 export ROS_DOMAIN_ID=0
 export ROS_WS=$HOME/ros2_ws/$ROS_DISTRO
-source /opt/ros/iron/setup.zsh
+source /opt/ros/$ROS_DISTRO/setup.zsh
 source $ROS_WS/install/local_setup.zsh
 source /usr/share/colcon_cd/function/colcon_cd.sh
 source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.zsh
