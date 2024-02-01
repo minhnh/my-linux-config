85ac968c1051772c83824ecba9efd6d5f89e1b03
Minh Nguyen <1168534+minhnh@users.noreply.github.com>
2024-02-01 16:43:14 +0100

    add IsaacSim configs

---
+++ env_config/isaac-sim.sh
@@ -0,0 +1,4 @@
+ISAAC_SIM_DIR="/media/ext4-data/admin/nvidia/ov/pkg/isaac_sim-2022.2.1"
+alias isaacsim-python.sh="${ISAAC_SIM_DIR}/python.sh"
+alias isaacsim-ipython="${ISAAC_SIM_DIR}/python.sh ${ISAAC_SIM_DIR}/kit/python/bin/ipython"
+alias isaacsim-behave="${ISAAC_SIM_DIR}/python.sh ${ISAAC_SIM_DIR}/kit/python/bin/behave"
