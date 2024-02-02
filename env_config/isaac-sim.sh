5fe995ab6bb8c05f70f6fe79b7997e02b6ddb717
Minh Nguyen <minh.nguyen@uni-bremen.de>
2024-02-02 11:06:21 +0100

    add isaac sim version

--- env_config/isaac-sim.sh
+++ env_config/isaac-sim.sh
@@ -1,4 +1,5 @@
-ISAAC_SIM_DIR="/media/ext4-data/admin/nvidia/ov/pkg/isaac_sim-2022.2.1"
-alias isaacsim-python.sh="${ISAAC_SIM_DIR}/python.sh"
+ISAAC_VER='2023.1.1'
+ISAAC_SIM_DIR="/media/ext4-data/admin/nvidia/ov/pkg/isaac_sim-${ISAAC_VER}"
+alias isaacsim-python-sh="${ISAAC_SIM_DIR}/python.sh"
 alias isaacsim-ipython="${ISAAC_SIM_DIR}/python.sh ${ISAAC_SIM_DIR}/kit/python/bin/ipython"
 alias isaacsim-behave="${ISAAC_SIM_DIR}/python.sh ${ISAAC_SIM_DIR}/kit/python/bin/behave"
