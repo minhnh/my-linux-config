dfbc2574298cb18b168ab7d0a3f65eca1aec2b38
Minh Nguyen <minh.nguyen@uni-bremen.de>
2024-02-02 11:33:04 +0100

    add alias for pip with Isaac

--- env_config/isaac-sim.sh
+++ env_config/isaac-sim.sh
@@ -1,5 +1,7 @@
 ISAAC_VER='2023.1.1'
-ISAAC_SIM_DIR="/media/ext4-data/admin/nvidia/ov/pkg/isaac_sim-${ISAAC_VER}"
-alias isaacsim-python-sh="${ISAAC_SIM_DIR}/python.sh"
-alias isaacsim-ipython="${ISAAC_SIM_DIR}/python.sh ${ISAAC_SIM_DIR}/kit/python/bin/ipython"
-alias isaacsim-behave="${ISAAC_SIM_DIR}/python.sh ${ISAAC_SIM_DIR}/kit/python/bin/behave"
+ISAAC_SIM_DIR="/media/ext4-data/shared/nvidia/ov/pkg/isaac_sim-${ISAAC_VER}"
+ISAAC_PYTHON_SH="${ISAAC_SIM_DIR}/python.sh"
+alias isaacsim-python-sh="${ISAAC_PYTHON_SH}"
+alias isaacsim-pip="${ISAAC_PYTHON_SH} -m pip"
+alias isaacsim-ipython="${ISAAC_PYTHON_SH} ${ISAAC_SIM_DIR}/kit/python/bin/ipython"
+alias isaacsim-behave="${ISAAC_PYTHON_SH} ${ISAAC_SIM_DIR}/kit/python/bin/behave"
