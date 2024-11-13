f92531e4dc3edaf054371207a45d1ae3c67fcd16
Minh Nguyen <minh.nguyen@uni-bremen.de>
2024-11-13 12:30:06 +0100

    add ros ws to ps1, roscd alias

--- env_config/ros2.sh
+++ env_config/ros2.sh
@@ -1,5 +1,7 @@
-ROS_WS_HOME="$HOME/ws/ros"
+ROS_WS_ROOT="$HOME/ws/ros"
 ROS_DISTRO="jazzy"
+# zsh handles new lines differently
+NEWLINE=$'\n'
 
 # fix argcomplete for ros2 & colcon in zsh
 # see https://github.com/ros2/ros2cli/issues/534
@@ -11,6 +13,9 @@ fix_zsh_autocomplete () {
 # source worskapce with just system underlay
 source_ros_ws () {
     source "/opt/ros/$ROS_DISTRO/setup.zsh"
-    source "$ROS_WS_HOME/$ROS_DISTRO/$1/install/setup.zsh"
+    export ROS_WS_HOME="$ROS_WS_ROOT/$ROS_DISTRO/$1"
+    alias roscd="cd $ROS_WS_HOME/src"
+    source "$ROS_WS_HOME/install/setup.zsh"
     fix_zsh_autocomplete
+    export PS1="$NEWLINE($ROS_DISTRO/$1)$PS1"
 }
