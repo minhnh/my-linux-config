ad0813b4bddff1b72cda781b23544041a260dfde
Minh Nguyen <1168534+minhnh@users.noreply.github.com>
2026-02-04 17:21:14 +0100

    improve ros2 setup func
    - get optionally ros distro from arguments
    - export ros distro
    - check if setup file and, if not, exit before exporting variables
    - print usage if no args supplied

--- env_config/ros2.sh
+++ env_config/ros2.sh
@@ -1,5 +1,3 @@
-ROS_WS_ROOT="$HOME/ws/ros"
-ROS_DISTRO="jazzy"
 # zsh handles new lines differently
 NEWLINE=$'\n'
 
@@ -12,10 +10,39 @@ fix_zsh_autocomplete () {
 
 # source worskapce with just system underlay
 source_ros_ws () {
-    source "/opt/ros/$ROS_DISTRO/setup.zsh"
-    export ROS_WS_HOME="$ROS_WS_ROOT/$ROS_DISTRO/$1"
-    alias roscd="cd $ROS_WS_HOME/src"
+    SCRIPT_NAME=$0
+    ROS_WS_ROOT="$HOME/ws/ros2"
+    ROS_DISTRO="kilted"
+
+    usage() {
+        echo "Soruce ROS workspace in '$ROS_WS_ROOT'$NEWLINE"
+        echo "usage: $SCRIPT_NAME <ros_ws_name> <(optional)distro_name>"
+    }
+
+    if [ $# -lt 1 ] ; then
+        echo "no argument provided"
+        usage
+        return 1
+    fi
+
+    ENV_NAME=$1
+
+    if [ $# -gt 1 ] ; then
+        ROS_DISTRO=$2
+    fi
+
+    ROS_WS_HOME="$ROS_WS_ROOT/$ROS_DISTRO/$ENV_NAME"
+
+    SETUP_FILE="$ROS_WS_HOME/install/setup.zsh"
+    if [ ! -f $SETUP_FILE ] ; then
+        echo "setup file doesn't exit: $SETUP_FILE"
+        return 1
+    fi
+
     source "$ROS_WS_HOME/install/setup.zsh"
+    export ROS_WS_HOME
+    export ROS_DISTRO
+    alias roscd="cd $ROS_WS_HOME/src"
     fix_zsh_autocomplete
-    export PS1="$NEWLINE($ROS_DISTRO/$1)$PS1"
+    export PS1="$NEWLINE($ROS_DISTRO/$ENV_NAME)$PS1"
 }
