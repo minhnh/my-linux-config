a766e7b98074973c6f4974ec165cdce93bd5d2cf
Minh Nguyen <minh.nguyen@uni-bremen.de>
2026-08-05 11:27:24 +0200

    improve isaac sim export
    - change to a function that takes root isaac sim as an arg
    - check major version from $root/VERSION file
    - source ROS directly before starting if version 6 or newer
    - otherwise export LD_LIBRARY_PATH manually

--- env_config/isaac-sim.sh
+++ env_config/isaac-sim.sh
@@ -1,7 +1,36 @@
-ISAAC_VER='4.2.0'
-ISAAC_SIM_DIR="/media/ext4-data/shared/nvidia/ov/pkg/isaac-sim-${ISAAC_VER}"
-ISAAC_PYTHON_SH="${ISAAC_SIM_DIR}/python.sh"
-alias isaacsim-python-sh="${ISAAC_PYTHON_SH}"
-alias isaacsim-pip="${ISAAC_PYTHON_SH} -m pip"
-alias isaacsim-ipython="${ISAAC_PYTHON_SH} ${ISAAC_SIM_DIR}/kit/python/bin/ipython"
-alias isaacsim-behave="${ISAAC_PYTHON_SH} ${ISAAC_SIM_DIR}/kit/python/bin/behave"
+export_isaac_ros () {
+    ISAAC_SIM_ROOT=$1
+    if [ -z "$ISAAC_SIM_ROOT" ]; then
+        ISAAC_SIM_ROOT="/media/ext4-data/shared/nvidia/isaacsim/current"
+    fi
+    if [ ! -d "$ISAAC_SIM_ROOT" ]; then
+        echo "IsaacSim root '$ISAAC_SIM_ROOT' does not exist."
+        return 1
+    fi
+    ISAAC_SIM_ROOT=$(realpath -s "$ISAAC_SIM_ROOT")
+    echo "Using IsaacSim in: ${ISAAC_SIM_ROOT}"
+    ISAAC_SIM_VERSION=$(<"$ISAAC_SIM_ROOT/VERSION")
+    ISAAC_SIM_MAJOR=${ISAAC_SIM_VERSION%%.*}
+    echo "Isaac Sim major version: $ISAAC_SIM_MAJOR"
+
+    ROS_VER=$2
+    if [ -z "$ROS_VER" ]; then
+      ROS_VER="jazzy"
+    fi
+    export ROS_DISTRO="$ROS_VER"
+    echo "ROS version: $ROS_DISTRO"
+
+    ISAAC_PYTHON_SH="${ISAAC_SIM_ROOT}/python.sh"
+    alias isaacsim="${ISAAC_SIM_ROOT}/isaac-sim.sh"
+    alias isaacsim-python-sh="${ISAAC_PYTHON_SH}"
+    alias isaacsim-pip="${ISAAC_PYTHON_SH} -m pip"
+    alias isaacsim-ipython="${ISAAC_PYTHON_SH} ${ISAAC_SIM_ROOT}/kit/python/bin/ipython"
+    alias isaacsim-behave="${ISAAC_PYTHON_SH} ${ISAAC_SIM_ROOT}/kit/python/bin/behave"
+
+    export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
+    if (( ISAAC_SIM_MAJOR >= 6 )); then
+        source "/opt/ros/${ROS_DISTRO}/setup.zsh"
+    else
+        export LD_LIBRARY_PATH="${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}${ISAAC_SIM_ROOT}/exts/isaacsim.ros2.bridge/${ROS_DISTRO}/lib"
+    fi
+}
